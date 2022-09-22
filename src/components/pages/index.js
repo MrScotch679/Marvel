d21d13d7709f85d9b843d15dc189c0755d483e5c
Vlad <vladis67948@gmail.com>
2022-09-22 13:26:43 +0300

    Updates

--- src/components/pages/index.js
+++
@@ -1,7 +0,0 @@
-import MainPage from "./MainPage";
-import ComicsPage from "./ComicsPage";
-import Page404 from "./404";
-import SingleComicPage from "./singleComicPage/SingleComicPage";
-import SingleCharacterPage from "./singleCharacterPage/SingleCharacterPage";
-
-export {MainPage, ComicsPage, Page404, SingleComicPage, SingleCharacterPage};
